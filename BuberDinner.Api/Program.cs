211c8a8675a0676614a61f3392554e7ee44e3301
ayoub chaib <61378289+Vanhayten@users.noreply.github.com>
2023-10-03 15:37:21 +0100

    refactor gitignore

---
+++ BuberDinner.Api/Program.cs
@@ -0,0 +1,15 @@
+using BuberDinner.Application.Services.Authentication;
+
+var builder = WebApplication.CreateBuilder(args);
+{
+    builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+    builder.Services.AddControllers();
+}
+
+var app = builder.Build();
+
+{
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
+}
